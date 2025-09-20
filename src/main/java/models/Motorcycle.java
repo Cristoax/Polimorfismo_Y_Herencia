cac0fe2df1036b30a3051c581a0f49056afd0cd8
Kevinmedi <kmedina070207@gmail.com>
2025-09-20 11:48:04 -0600

    Agrego cambios a las clases vehicles, sus subclases y la interfaz fuel

--- src/main/java/models/Motorcycle.java
+++ src/main/java/models/Motorcycle.java
@@ -1,4 +1,15 @@
 package models;
 
-public class Motorcycle {
+public class Motorcycle extends Vehicles
+{
+    public Motorcycle(String brand, String model){
+        super(brand,model);
+    }
+
+    @Override
+    public void move(){
+
+        System.out.println(" The motorcycle " +getBrand()+ " Model " +getModel()+ " is moving rapidly through the street ");
+    }
+
 }
--- src/main/java/models/Vehicles.java
+++ src/main/java/models/Vehicles.java
@@ -1,4 +1,30 @@
 package models;
 
-public class Vehicles {
+public abstract class Vehicles {
+    private String brand;
+    private String model;
+
+    public Vehicles(String brand, String model)
+    {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public abstract void move();
+
+    public String getBrand() {
+        return brand;
+    }
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void ShowInfo()
+    {
+        System.out.println(" Brand: " + getBrand());
+        System.out.println(" Model: " + getModel());
+    }
+
 }
